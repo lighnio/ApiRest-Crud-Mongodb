0da807d81446955dfcd9bec765da669aeceae048
lighnio <charlesvas16@outlook.com>
2022-08-26 19:14:38 -0600

    super bad practice, env variablesss

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT || 3000;
+
+export const DB_URI = "mongodb+srv://lighnio-cv:S4dErin1Rr9QbCEQ@nodeexpressmongocrud.lgazcpd.mongodb.net/?retryWrites=true&w=majority"
