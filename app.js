44d265d12e654980fa1a0f00147ac00aaad314ab
lighnio <charlesvas16@outlook.com>
2022-08-27 01:50:29 -0600

    ready base for tracks and storage

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import { dbConnect } from "./src/config/mongo.js";
 import { PORT } from './config.js';
-import {dbConnect} from './src/config/mongo.js'
-import router from './src/routes/tracks.js';
+import router from './src/routes/routes.js';
 
 
 const app = express();
 
 app.use(cors())
+app.use(express.json())
+app.use(express.static("./src/storage"))
 app.use('/api/', router)
 app.listen(PORT)
 console.log("SERVER ON PORT: ", PORT);
