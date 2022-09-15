438f0a4bdd7dc96d8baf12a1b06c8632d77ec8af
lighnio <charlesvas16@outlook.com>
2022-09-15 17:14:45 -0600

    added new function to detect execution mode

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import swaggerUI from 'swagger-ui-express';
 import {openApiConf} from './docs/swagger.js'
 import { dbConnect as dbConnectNoSql} from "./src/config/mongo.js";
 import { dbConnectMySql } from './src/config/mysql.js';
-import { ENGINE_BD, PORT } from './config.js';
+import { ENGINE_BD, PORT, NODE_ENV } from './config.js';
 import router from './src/routes/routes.js';
 
 const app = express();
@@ -15,8 +15,11 @@ app.use(express.json());
 app.use(express.static("./src/storage"));
 app.use('/api/', router);
 app.use('/documentation', swaggerUI.serve, swaggerUI.setup(openApiConf))
-app.listen(PORT);
+
+NODE_ENV !== 'test'? app.listen(PORT) : '';
 
 ENGINE_BD === 'nosql'? dbConnectNoSql() : dbConnectMySql();
 
 console.log("SERVER ON PORT: ", PORT);
+
+export default app;
