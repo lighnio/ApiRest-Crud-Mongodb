cde09f00dc6230602b8779330dda9bcaa6aa7dc5
lighnio <charlesvas16@outlook.com>
2022-09-15 17:21:43 -0600

    contains all samples for the testing

---
+++ test/helpers/dataHelper.js
@@ -0,0 +1,52 @@
+export const testAuthRegister = {
+  name: "User Test",
+  age: 21,
+  email: "test1@test1.com",
+  password: "MyPass01",
+};
+
+export const testAuthLogin = {
+  email: "test1@test1.com",
+  password: "MyPass01",
+};
+
+export const storageTestAuthRegister = {
+  name: "User Test",
+  age: 21,
+  email: "test2@test2.com",
+  password: "MyPass01",
+};
+
+export const storageTestAuthLogin = {
+  email: "tes2t@test2.com",
+  password: "MyPass01",
+};
+
+export const testAuthRegisterAdmin = {
+  name: "Admin Test",
+  age: 20,
+  email: "test@test.com",
+  role: ["admin"],
+  password: "MyPass01",
+};
+
+export const testStorageRegister = {
+  url: "http://localhost:3000/image.jpg",
+  filename: "image.jpg",
+};
+
+export const testDataTrack = {
+  name: "CV",
+  album: "CV Example",
+  cover: "http://lighnio.github.io",
+  artist: {
+    name: "Lighnio2",
+    nickname: "CV2",
+    nationality: "GT",
+  },
+  duration: {
+    start: 1,
+    end: 10,
+  },
+  mediaId: "",
+};
